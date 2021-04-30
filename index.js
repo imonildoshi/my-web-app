e95cab9976761ddde18a8311477d4d715e3ba179
Monil Doshi <monil@yalochat.com>
2021-04-30 19:20:26 +0530

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const express = require('express');
+const redisClient = require('./redis-client');
 
-// Constants
-const PORT = 8080;
+const PORT = process.env.PORT;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/store/:key', async (req, res) => {
+  const {key} = req.params;
+  const value = req.query;
+  await redisClient.setAsync(key, JSON.stringify(value));
+  return res.send('Success');
+});
+
+app.get('/:key', async (req, res) => {
+  const {key} = req.params;
+  const rawData = await redisClient.getAsync(key);
+  return res.json(JSON.parse(rawData));
+});
+
 app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
